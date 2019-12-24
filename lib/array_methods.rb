d685f887d4cc0e15e723ae94fb9b4e3d15345c23
Miwha Geschwind <miwhag@gmail.com>
2019-12-24 13:32:05 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,9 +10,15 @@ end
 
 find_element_index([1,2,3,4],4)
 
+
+
 def find_max_value(array)
-  # Add your solution here
+ puts array.max 
 end
+
+find_max_value([])
+
+
 
 def find_min_value(array)
   # Add your solution here
